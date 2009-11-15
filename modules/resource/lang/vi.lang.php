8309bdae2abbbc6193017701ac6e8ac73eaf969e
ducduydaovn <ducduydaovn@e041bba5-cfa8-ea9b-5892-9dc1d484c9c9>
2009-11-15 08:11:49 +0000

    Added Vietnamese Language
    git-svn-id: http://xe-resource.googlecode.com/svn/sandbox@33 e041bba5-cfa8-ea9b-5892-9dc1d484c9c9

---
+++ modules/resource/lang/vi.lang.php
@@ -0,0 +1,106 @@
+<?php
+    $lang->resource_manager = 'Quản lý tài nguyên';
+    $lang->about_resource_manager = 'Module quản lý tài nguyên.';
+
+    $lang->order_newest = "Bài mới gửi";
+    $lang->order_latest = "Mới cập nhật";
+    $lang->order_popular = "Top";
+    $lang->package_status = 'Trạng thái';
+
+    $lang->download = 'Download';
+    $lang->package_downloaded_count = 'Download';
+    $lang->downloaded_count = 'Download';
+    $lang->voted_count = 'Bình chọn';
+    $lang->resource_estimate = 'Đánh giá';
+    $lang->star_point = 'Cho điểm';
+    $lang->cmd_estimate = 'Đánh giá';
+    $lang->msg_already_voted = 'Bạn đã đánh giá xong. Xin cảm ơn!';
+
+    $lang->cmd_list_my_package = 'Dữ liệu của tôi';
+    $lang->about_my_package = 'Danh sách tất cả những dữ liệu do bạn gửi, và được phân phối trực tiếp tại đây.';
+
+    $lang->package_accepted = 'Hợp lệ';
+    $lang->package_waiting = 'Đang chờ duyệt';
+    $lang->package_reservation = 'Dự phòng';
+
+    $lang->cmd_attach_file = 'Đính kèm';
+    $lang->package_author = 'Tác giả';
+
+    $lang->cmd_view_all_version = 'Tất cả các phiên bản';
+    $lang->version_history = 'Danh sách các phiên bản';
+    $lang->latest_version = 'Phiên bản mới';
+    $lang->selected_version = 'Phiên bản đã chọn';
+
+    $lang->attach_version = 'Phiên bản';
+    $lang->about_attach_version = 'Xin hãy nhập phiên bản.<br/>Định dạng của phiên bản thường có dạng a.b.c.<br/>c là những thay đổi nhỏ, a là những thay đổi lớn từ phiên bản trước.<br/>Qua số phiên bản giúp người sử dụng có thể hiểu được những thay đổi lớn hay nhỏ từ những phiên bản trước đó.';
+    $lang->latest_attach_version = 'Phiên bản mới nhất';
+
+    $lang->attach_file = 'Đính kèm';
+    $lang->about_attach_file = 'Xin hãy chọn File bạn muốn đính kèm.<br/>zip, tgz, tar là những định dạng File được khuyến khích sử dụng.';
+
+    $lang->attach_description = 'Mô tả File';
+    $lang->about_attach_description = 'Xin hãy nhập mô tả cho File đính kèm.';
+
+    $lang->no_packages = 'Không có dữ liệu được đăng kí.';
+
+    $lang->package_version = 'Phiên bản';
+
+    $lang->search_dependency = 'Tìm kiếm';
+    $lang->attach_dependency = 'Dữ liệu đã đính kèm';
+    $lang->about_attach_dependency = 'File đính kèm của bạn có thể trùng tên với File khác do người khác đã gửi lên.<br/>File Download có thể nhận được những hướng dẫn cụ thể.';
+
+    $lang->attach_screenshot = 'Hình mô tả';
+    $lang->about_attach_screenshot = 'Xin hãy nhập hình mô tả cho dữ liệu của bạn.<br/>100x100px là định dạng chuẩn cho hình mô tả.<br/>Chỉ hỗ trợ những định dạng .jpg, .gif, .png.';
+
+    $lang->cmd_manage_file = 'Quản lý File';
+    $lang->about_manage_file = 'Sau khi dữ liệu muốn gửi của bạn được chấp nhận là hợp lệ, bạn có thể Upload trực tiếp File tại đây.<br/>Nếu dữ liệu đính kèm do người khác gửi lên, bạn phải công khai nguồn gốc cho người sử dụng.<br/>File đính kèm của bạn sẽ được chúng tôi trực tiếp quản lý.';
+
+    $lang->cmd_manage_package = 'Xem xét dữ liệu';
+    $lang->about_manage_package = 'Có thể xét là hợp lệ, dự phòng hay xóa dữ liệu do người dùng gửi lên.';
+
+    $lang->cmd_insert_package = 'Đăng kí dữ liệu';
+    $lang->about_insert_package= 'Phải nhập những thông số bắt buộc cho dữ liệu trước khi đăng kí.<br/>Sau khi nhập vào những thông số hợp lệ cho dữ liệu, dữ liệu của bạn sẽ được đăng kí.<br/>Phải nêu rõ dữ liệu này được sử dụng riêng biệt hay sử dụng kết hợp với những dữ liệu khác.<br/>Sau khi hoàn tất, hãy bấm nút <b>[Gửi]</b> phía dưới.';
+
+    $lang->cmd_modify_package = 'Sửa dữ liệu';
+    $lang->about_modify_package = 'Bạn có thể sửa những thông số cho dữ liệu như Tên, Giấy phép, Trang chủ, Mô tả, ... một cách đơn giản.';
+
+    $lang->cmd_delete_package = 'Xóa dữ liệu';
+    $lang->about_delete_package= 'Sau khi xóa dữ liệu, File đính kèm cũng đồng thời được xóa.';
+
+    $lang->about_required_field = 'Tùy chọn <em>Yêu cầu nhập vào</em>.';
+    $lang->required_field = 'Bắt buộc phải nhập yêu cầu này.';
+
+    $lang->package_regdate = 'Ngày gửi';
+    $lang->package_update = 'Ngày cập nhật';
+
+    $lang->package_category = 'Thể loại';
+    $lang->about_package_category = 'Hãy chọn thể loại cho dữ liệu muốn gửi.';
+
+    $lang->package_title = 'Tiêu đề';
+    $lang->about_package_title = 'Xin hãy nhập tiêu đề cho dữ liệu muốn gửi.';
+
+    $lang->package_path = 'Đường dẫn cài đặt';
+    $lang->about_package_path = 'Hãy nhập ./modules/tên module giống với tên dữ liệu cần Upload trước khi cài đặt.';
+
+    $lang->package_license = 'Giấy phép';
+    $lang->about_package_license = 'Hãy chọn giấy phép được cấp cho dữ liệu.<br/>Nếu là giấy phép khác, xin hãy mô tả đơn giản giấy phép đó.';
+
+    $lang->package_homepage = 'Trang chủ';
+    $lang->about_package_homepage = 'Hãy nhập trang chủ đã cài đặt thành công dữ liệu này.';
+
+    $lang->package_description = 'Giới thiệu tóm tắt';
+    $lang->about_package_description = 'Hãy giới thiệu tóm tắt về dữ liệu muốn gửi.<br/>Giới thiệu này sẽ hiển thị kèm với thông tin của dữ liệu.<br/>Cho phép tối đa 300 kí tự.';
+
+    $lang->resource_notify_mail = 'Email thông báo';
+    $lang->about_resource_notify_mail = 'Sẽ gửi một thông báo tới Email này khi có dữ liệu được đăng kí hay sử đổi.';
+
+    $lang->resource_use_path = 'Đường dẫn sử dụng dữ liệu';
+    $lang->about_resource_use_path = 'Nếu không sử dụng, bạn sẽ không nhận được thông tin đường dẫn sử dụng cho dữ liệu này.';
+
+    $lang->resource_new_notify_title = 'Dữ liệu mới đã được gửi.';
+    $lang->resource_modify_notify_title = 'Thông tin dữ liệu đã được thay đổi.';
+    $lang->resource_delete_notify_title = 'Dữ liệu đã được xóa.';
+    $lang->resource_attach_notify_title = 'Dữ liệu đã được đính kèm.';
+    $lang->resource_status_changed = 'Trạng thái dữ kiệu đã được thay đổi.';
+    $lang->resource_status_changed_message = 'Dữ liệu [title] đã chuyển sang trạng thái [status].';
+?>
