bf353c850404d918cda71eeec6bd85fa8917b14e
ngleader <ngleader@gmail.com>
2011-02-23 09:48:53 +0000

    r505 add Turkey langauge
    git-svn-id: http://xe-resource.googlecode.com/svn/trunk@76 e041bba5-cfa8-ea9b-5892-9dc1d484c9c9

---
+++ modules/resource/lang/tr.lang.php
@@ -0,0 +1,108 @@
+<?php
+
+$lang->resource_manager = 'Kaynak Yönetimi';
+$lang->about_resource_manager = 'Bu modül indirilebilir verileri yönetme yetkisine sahiptir.';
+
+$lang->order_newest = "Kayıt";
+$lang->order_latest = "Son Güncellemeler";
+$lang->order_popular = "Popülarite";
+$lang->order_download = "İndirilenler";
+$lang->package_status = 'Paket Statüleri';
+
+$lang->download = 'İndir';
+$lang->package_downloaded_count = 'İndirilen Paketler';
+$lang->downloaded_count = 'İndirme Sayısı';
+$lang->voted_count = 'Oylandı.';
+$lang->resource_estimate = 'Oran';
+$lang->star_point = 'Sınıflandırma';
+$lang->cmd_estimate = 'Değerlendir';
+$lang->msg_already_voted = 'Zaten değerlendirmede bulunmuştunuz.';
+
+$lang->cmd_list_my_package = 'Listem';
+$lang->about_my_package = '"Kayıtlı" kaynağın onaylandığı anlamına gelir.<br /> Onaylanmış kaynaklara dosya ekleyebilirsiniz.';
+
+$lang->package_accepted = 'Onaylandı';
+$lang->package_waiting = 'Bekliyor';
+$lang->package_reservation = 'Rezervasyon';
+
+$lang->cmd_attach_file = 'Dosya Ekle';
+$lang->package_author = 'Yapımcı';
+
+$lang->cmd_view_all_version = 'Tüm sürümleri gör';
+$lang->version_history = 'Sürüm Geçmişi';
+$lang->latest_version = 'Son Sürüm';
+$lang->selected_version = 'Seçili Sürüm';
+
+$lang->attach_version = 'Sürüm';
+$lang->about_attach_version = 'Sürümü girin. <br /> a.b.c.d şeklinde biçimlendirilmelidir(c, d çıkarılabilir).';
+$lang->latest_attach_version = 'Son Sürüm';
+
+$lang->attach_file = 'Ekler';
+$lang->about_attach_file = 'Dosyaları Yükle. <br /> zip, tgz, tar, vb. popüler sıkıştırma formatları kullanmanızı tavsiye ederiz';
+
+$lang->attach_description = 'Dosya Tanım';
+$lang->about_attach_description = 'Dosyanı yükle ve ilgili tanımı yaz.';
+
+$lang->no_packages = 'Paket yok. <br /> Paket kaydedebilirsiniz.';
+
+$lang->package_version = 'Sürüm';
+
+$lang->search_dependency = 'Arama Bağımlılığı';
+$lang->attach_dependency = 'Ekleme Bağımlılığı';
+$lang->about_attach_dependency = 'Eğer verileriniz diğer yüklü dosyalara bağlıysa, lütfen dosyaları belirtiniz. <br /> Böylece paketiniz bu dosyalarla birlikte indirilmeye yönlendirilecektir.';
+
+$lang->attach_screenshot = 'Ekran Görüntüsü';
+$lang->about_attach_screenshot = 'Dosyalar listesinde görüntülenecek ekran görüntüsünü kaydedin. <br /> 100x100 boyutları tavsiye edilir. <br /> Lütfen jpg, gif veya png formatlarını kullanın.';
+
+$lang->cmd_manage_file = 'Dosya Yönetimi';
+$lang->about_manage_file = 'Onaylı veriler için dosya yükleyebilirsiniz. <br /> Birbirine bağlı paketleri belirtmeniz indiricilere kolaylık sağlayacaktır. <br />Ekli dosyaları yönetebilirsiniz. ';
+
+$lang->cmd_manage_package = 'Paketi Yönet';
+$lang->about_manage_package = 'Yönenici, kayıtlı paketleri onaylayabilir/erteleyebilir/silebilir.';
+
+$lang->cmd_insert_package = 'Paket Ekle';
+$lang->about_insert_package= 'Dosyaları yüklemeden önce kaynağı kaydetmek zorundasınız. <br /> Onaylandıktan sonra dosyaları kaynağa ekleyebilirsiniz. <br /> Bunun nedeni, kullanıcılar indirmeden önce yöneticinin dosyaları sürümleri ve bağımlılıkları ile gözden geçirmesidir.<br /> Aşağıdakileri girin ve ardından kaydet düğmesine basın.';
+
+$lang->cmd_modify_package = 'Paketi Değiştir';
+$lang->about_modify_package = 'Onaylanmış kaynaklarda başlığı, lisansı, anasayfayı ve özet bilgileri değiştirebilirsiniz.';
+
+$lang->cmd_delete_package = 'Paketi Sil';
+$lang->about_delete_package= 'Eğer bu paketi silerseniz, tüm ekli dosyalar da silinecektir.';
+
+$lang->about_required_field = 'Bu <em>gerekli</em>.';
+$lang->required_field = 'Bu alanı doldurmanız gerekmektedir.';
+
+$lang->package_regdate = 'Paket Kayıt Tarihi';
+$lang->package_update = 'Son Güncellemeler';
+
+$lang->package_category = 'Kategori';
+$lang->about_package_category = 'Lütfen kategoriyi belirtiniz.';
+
+$lang->package_title = 'Yazı Başlığı';
+$lang->about_package_title = 'Lütfen başlığı girin';
+
+$lang->package_path = 'Kurulum Yolu';
+$lang->about_package_path = 'Kurulum yolunu belirtin. ör., ./modules/[modül\'ün adı]';
+
+$lang->package_license = 'Lisans';
+$lang->about_package_license = 'Lütfen lisansı belirtiniz. <br /> Eğer listede yoksa, vb.yi seçin ve ardından lisansı özet olarak tanıtın.';
+
+$lang->package_homepage = 'Anasayfa';
+$lang->about_package_homepage = 'Anasayfanızı veya proje URL\'sini girin.';
+
+$lang->package_description = 'Kısa Tanıtım';
+$lang->about_package_description = 'Paketinizi kısaca tanıtın. <br /> Özet bilgiler kaynak listesinde görüntülenecektir. <br /> 300 karakter genişliğinde bir tanıtım yapabilirsiniz.';
+
+$lang->resource_notify_mail = 'Posta Bildirimi';
+$lang->about_resource_notify_mail = 'Paketlerin kaydedilme/değiştirilme bildirimleri kayıtlı adresinize mail yoluyla gönderilecektir.';
+
+$lang->resource_use_path = 'Yol bilgisini kullanarak';
+$lang->about_resource_use_path = 'Eğer seçili değilse, yol bilgisi kullanılamayacaktır.';
+
+$lang->resource_new_notify_title = 'Veri kaydı gereklidir.';
+$lang->resource_modify_notify_title = 'Bu bilgi verileri değiştirebilir.';
+$lang->resource_delete_notify_title = 'Paket kaldırıldı.';
+$lang->resource_attach_notify_title = 'Bu paket dosyaya eklendi.';
+$lang->resource_status_changed = 'Kayıtlı verilerin statüleri değişti.';
+$lang->resource_status_changed_message = '[Başlık] statüsü [statüler] şekline dönüştü.';
+?>
